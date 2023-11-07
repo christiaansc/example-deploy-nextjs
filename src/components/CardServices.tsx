4bb06e050209ff127af5f7fbd74c550a3d4f1274
Christian sc <cristiaan.sc@gmail.com>
2023-11-07 13:40:30 -0300

    fix / tittle Gallery

--- src/components/CardServices.tsx
+++ src/components/CardServices.tsx
@@ -21,11 +21,11 @@ const CardServices = () => {
             <>
                 {
                     services.map((item, idx) => (
-                        <div key={idx} className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mt-5">
+                        <div key={idx} className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] mt-5">
 
                             <div className="p-6">
                                 <h5
-                                    className="mb-2 text-xl font-medium leading-tight text-green-950 dark:text-green-900">
+                                    className="mb-2 text-xl font-medium leading-tight text-green-950">
                                     {item.title}
                                 </h5>
                                 <p className="mb-4 text-base text-green-900 ">
